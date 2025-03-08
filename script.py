af625fb319223cc5101b5ca866dc5be06fa798d6
Davinci-Developer-Official <davincideveloper01@gmail.com>
2025-03-09 01:24:31 +0300

    09/03/2025.4

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("yes")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/first")
+def read_first():
+    print("hello fast api is running")
+    return {"message":"Hello,Fast Api is running"}
