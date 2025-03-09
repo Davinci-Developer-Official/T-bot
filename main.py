43e5d05447900c431539044168c981da1511907e
Davinci-Developer-Official <davincideveloper01@gmail.com>
2025-03-09 23:07:37 +0300

    09/03/2025.1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import MetaTrader5 as mt5
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 def read_first():
     print("hello fast api is running")
     return {"message":"Hello,Fast Api is running"}
+
+@app.get("/mt5")
+def init():
+    open = mt5.initialize()
+    return "mt5 opening ........"
